defb0b2efbcd4978cb6a1b884efddfedd8ff3f1e
Lucianodasilvameyer <murcilhax@hotmail.com>
2019-07-30 22:59:27 -0300

    menu do endless

---
+++ Assets/MainMenuScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuScript: MonoBehaviour
+{
+   public void PlayGame()
+   {
+        SceneManager.LoadScene("SampleScene");
+   }
+   public void quit()
+   {
+        Debug.Log("saiu");// só diz no console q saiu? //teste
+        Application.Quit();
+   }     
+
+}
